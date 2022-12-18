3e7c89a765c95624f17039abf03c0feb567da2a4
AlvesLuis0 <alves.luis.3002@gmail.com>
2022-12-18 16:15:24 -0300

    AI implemented

---
+++ src/eden.ts
@@ -0,0 +1,27 @@
+import { Configuration, OpenAIApi } from "openai";
+
+
+const config = new Configuration({
+  apiKey: "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Ik1UaEVOVUpHTkVNMVFURTRNMEZCTWpkQ05UZzVNRFUxUlRVd1FVSkRNRU13UmtGRVFrRXpSZyJ9.eyJodHRwczovL2FwaS5vcGVuYWkuY29tL3Byb2ZpbGUiOnsiZW1haWwiOiJpbnNlcnRjb2luMjAwM0BnbWFpbC5jb20iLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwiZ2VvaXBfY291bnRyeSI6IkJSIn0sImh0dHBzOi8vYXBpLm9wZW5haS5jb20vYXV0aCI6eyJ1c2VyX2lkIjoidXNlci1NeTloeFFwMlF0WEpyNWd1NmNGRGhKYUoifSwiaXNzIjoiaHR0cHM6Ly9hdXRoMC5vcGVuYWkuY29tLyIsInN1YiI6ImF1dGgwfDYzOWM3OTU2Yzg5NjllNmYyNTEzZjZlOCIsImF1ZCI6WyJodHRwczovL2FwaS5vcGVuYWkuY29tL3YxIiwiaHR0cHM6Ly9vcGVuYWkuYXV0aDAuY29tL3VzZXJpbmZvIl0sImlhdCI6MTY3MTI5OTY2MSwiZXhwIjoxNjcxOTA0NDYxLCJhenAiOiJUZEpJY2JlMTZXb1RIdE45NW55eXdoNUU0eU9vNkl0RyIsInNjb3BlIjoib3BlbmlkIHByb2ZpbGUgZW1haWwgbW9kZWwucmVhZCBtb2RlbC5yZXF1ZXN0IG9yZ2FuaXphdGlvbi5yZWFkIG9mZmxpbmVfYWNjZXNzIn0.iVIXRVY9FigNNBP_0mMuSpb5P6sMLXJTv1coSVhbJx7AgQGN4H4jk9wwIiiv_VDqYJS1wr1IiLrzjU09VK6GtCbJ2u8wxoPgXMnBkvTcmQsc410w5deB_P-R9EnyOM-dozuejhnESFX3MW7GF71PjaNKxQahUs1saRiT-n6eaZa7DYiDPaZovoLJtdje1N7MXR13FbVkT7NfTk8lNKS2REgFW-AUuoBwoEwET2gt3Ukc534dr27dMjoOFIk7KSFqh0MFHUyMzytU1dpWKe-xt9mCi-WwR80ITX1nzOw7KukTBr8nMa3FE3J9-obUCs_6fYisarNELpDcWL4GaF8J2w"
+});
+const eden = new OpenAIApi(config);
+
+
+export async function askQuestion(question:string) {
+  try {
+    const response = await eden.createCompletion({
+      model: "text-davinci-002",
+      prompt: question,
+      temperature: 0,
+      max_tokens: 4000
+    }, {
+      timeout: 30 * 1000
+    });
+
+    return response.data.choices[0].text;
+  }
+
+  catch(error) {
+    return "Error: " + error.response.data.error.message;
+  }
+}
--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,27 @@
 import { Client } from "discord.js";
+import { askQuestion } from "./eden";
 const bot = new Client({ intents: [1,512,32768] });
 
 
-bot.on("messageCreate", async(ctx) => {
+bot.on("messageCreate", async(ctx): Promise<any> => {
 
   // if(!ctx.content.startsWith("//") || ctx.author.bot) return;
-  
-  
-  const message = ctx.content;
+  const message = ctx.content.toLowerCase();
+  const messageWithoutPrefix = message.slice(2);
+
+
+  if(message.startsWith("//")) {
+    try { ctx.reply(await askQuestion(messageWithoutPrefix)) }
+    catch { ctx.reply("Sei não man") } 
+    return;
+  }
   
 
   for(const i of ["gay","viado","tchola","biba","baitola"])
     if(message.includes(i))
-      ctx.reply(":rainbow_flag:?");
+      return ctx.reply(":rainbow_flag:?");
 
 });
 
 
-bot.login("MTAyNjU1NjUzODc5OTc4NDAyOA.G6vA9_.ZWtBPy60aeeEiX3QGBzdya43pGKUpFvbbhxHiI");
+bot.login("M$TAyNjU1NjUzODc5OTc4NDAyOA.GsbFYL.o9u9p4Nod6NpC0e5YT1AR1SzSnMkA7FjilhvfQ");
